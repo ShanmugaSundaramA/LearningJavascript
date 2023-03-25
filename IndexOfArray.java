c44afbe23b6ea0beddfd3cc8e8e71775132614eb
ShanmugaSundaram <sundarvikram08@gmail.com>
2023-03-25 03:40:46 +0000

    Little changes

--- IndexOfArray.java
+++ IndexOfArray.java
@@ -3,38 +3,39 @@ import java.util.Scanner;
 class IndexOfArray{
  public static void main(String[] args) {
         int[] arr={1,2,3,5,4,7,6,8,9};
-        Scanner scan=new Scanner(System.in);
-        int input=scan.nextInt();
-        int num=input;
-        int count=-1;
-        while (num>0){
-            count++;   
-            num=(int)num/10; 
-        }
-
-       //Finding first no, index position.
-       int output=0;
-       int firstNo=(int)(input/(Math.pow(10, count)));
-       for(int i=0;i<arr.length;i++){
-             if(firstNo==arr[i]){
-                 output=i;
-             }
-       }
-       input=(int)(input%(Math.pow(10, count)));
-       count--;
-       int j=0;
-       while(input>0){
-            int No=(int)(input/(Math.pow(10, count)));
-            for(int i=0;i<arr.length;i++){
-                if(No==arr[i]){
-                        j+=i-output;
-                        output=i;
-                        System.out.println(j);
-                }
+        try (Scanner scan = new Scanner(System.in)) {
+            int input=scan.nextInt();
+            int num=input,count=-1;
+            while (num>0){
+                count++;   
+                num=(int)num/10; 
             }
-            input=(int)(input%(Math.pow(10, count)));
-            count--;
-       }
-       System.out.println("output : "+j);
+
+      //Finding first no, index position.
+      int output=0;
+      int firstNo=(int)(input/(Math.pow(10, count)));
+      for(int i=0;i<arr.length;i++){
+                 if(firstNo==arr[i]){
+                     output=i;
+                 }
+      }
+      input=(int)(input%(Math.pow(10, count)));
+      count--;
+      //Finding positions...
+      int j=0;
+      while(input>0){
+                int No=(int)(input/(Math.pow(10, count)));
+                for(int i=0;i<arr.length;i++){
+                    if(No==arr[i]){
+                            j+=i-output;
+                            output=i;
+                            System.out.println(j);
+                    }
+                }
+                input=(int)(input%(Math.pow(10, count)));
+                count--;
+      }
+      System.out.println("output : "+j);
+        }
     }
 }
